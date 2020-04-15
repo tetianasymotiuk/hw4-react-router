cbfec7cb1ccce1adf6d7dfe8e39dc0ba2d9949f6
Tetiana <tetianaprivat@gmail.com>
2020-04-15 13:55:29 +0300

    Updates

---
+++ precache-manifest.ad8bbb6b4427f9f215245ea545a589e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143557c7b05abcdcea5469004f422ed3",
+    "url": "/hw4-react-router/index.html"
+  },
+  {
+    "revision": "d3bc6d789bc679c8bcef",
+    "url": "/hw4-react-router/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a52c3f4739be12135f3e",
+    "url": "/hw4-react-router/static/js/2.a9a9472d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw4-react-router/static/js/2.a9a9472d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3bc6d789bc679c8bcef",
+    "url": "/hw4-react-router/static/js/main.13a7cda1.chunk.js"
+  },
+  {
+    "revision": "6e2a9629adaf586b5170",
+    "url": "/hw4-react-router/static/js/runtime-main.eacc8c57.js"
+  }
+]);
